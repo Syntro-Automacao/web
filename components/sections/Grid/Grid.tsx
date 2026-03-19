79d97ed5bd3921e8b508eb25d8560f36acca7d3b
Nelly Arias <nelly.a.arias@gmail.com>
2026-03-19 09:39:43 -0300

    atualização de images do grid

--- components/sections/Grid/Grid.tsx
+++ components/sections/Grid/Grid.tsx
@@ -29,13 +29,13 @@ export default function Grid() {
     "/assets/images/iot/linha_montagem.webp",
     "/assets/images/iot/proceso_enchimento.webp",
     "/assets/images/iot/planta_quimica.webp",
-    "/assets/images/iot/proceso_enchimento.webp",
-    "/assets/images/iot/sistema_supervisorio2.webp",
+    "/assets/images/iot/sistema_supervisorio.webp",
+    "/assets/images/iot/painel_solar.webp",
     "/assets/images/iot/sistema_supervisorio.webp",
     "/assets/images/iot/planta_quimica.webp",
-    "/assets/images/iot/proceso_enchimento.webp",
+    "/assets/images/iot/reservatorio_agua.webp",
     "/assets/images/iot/supervisorio_frezer2.webp",
-    "/assets/images/iot/sistema_supervisorio.webp",
+    "/assets/images/iot/supervisorio_freezer3.webp",
     "/assets/images/iot/supervisorio_freezer3.webp",
   ];
 
