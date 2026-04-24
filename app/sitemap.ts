fcb92dc9b70c66c97a39839171182bf9e60618f1
Nelly Arias <nelly.a.arias@gmail.com>
2026-04-24 17:11:33 -0300

    adicionar link de sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -44,5 +44,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "monthly",
       priority: 0.7,
     },
+    {
+      url: `${baseUrl}/privacidade`,
+      lastModified: currentDate,
+      changeFrequency: "yearly",
+      priority: 0.3,
+    },
   ];
 }
